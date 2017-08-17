c2b51abb07596f6f4037a3c3685c2a0d035b293f
Paul-Andrew Joseph Miseiko <pmiseiko@rapid7.com>
2017-08-17 18:53:39 -0400

    Fix up to support both encrypt and decrypt.  Help the randomness hash when input data is repetitive.

--- encryption/hash/hash_encrypt.py
+++ encryption/hash/hash_encrypt.py
@@ -1,32 +1,53 @@
 import binascii
 import hashlib
+import string
 
 secret = "this is another secret"
 inData = b"The quick brown fox jumps over the lazy dog"
-outData = []
 
-secretHash = hashlib.sha3_256()
-secretHash.update(secret.encode())
-secretDigest = secretHash.digest()
-randomnessHash = hashlib.sha3_256()
-randomnessHash.update(secretDigest)
-
-inDataIndex = 0
-while inDataIndex < len(inData):
+def crypt(secret, inData, encrypt):
+ outData = []
+ secretHash = hashlib.sha256()
+ secretHash.update(secret.encode())
+ secretDigest = secretHash.digest()
+ randomnessHash = hashlib.sha256()
  randomnessHash.update(secretDigest)
- randomnessDigest = randomnessHash.digest()
- randomnessHash.update(randomnessDigest)
- randomnessIndex = 0
- while inDataIndex < len(inData) and randomnessIndex < len(randomnessDigest):
-  randomnessHash.update(chr(inData[inDataIndex]).encode())
-  outByte = inData[inDataIndex] ^ randomnessDigest[randomnessIndex]
-  outData.append(chr(outByte))
-  print("%c %02x (%03d) ^ %02x (%03d) = %02x" % (
-   inData[inDataIndex], inData[inDataIndex], inDataIndex,
-   randomnessDigest[randomnessIndex], randomnessIndex,
-   outByte))
-  inDataIndex += 1
-  randomnessIndex += 1
+
+ inDataIndex = 0
+ while inDataIndex < len(inData):
+  randomnessHash.update(secretDigest)
+  randomnessDigest = randomnessHash.digest()
+  randomnessHash.update(randomnessDigest)
+  randomnessIndex = 0
+  while inDataIndex < len(inData) and randomnessIndex < len(randomnessDigest):
+   if encrypt:
+    randomnessHash.update(chr((inDataIndex & 0xFF) ^ inData[inDataIndex]).encode())
+   outByte = inData[inDataIndex] ^ randomnessDigest[randomnessIndex]
+   if not encrypt:
+    randomnessHash.update(chr((inDataIndex & 0xFF) ^ outByte).encode())
+   outData.append(chr(outByte))
+   if encrypt:
+    print("%c 0x%02X (%03d) ^ 0x%02X (%03d) = 0x%02X" % (
+     inData[inDataIndex], inData[inDataIndex], inDataIndex,
+     randomnessDigest[randomnessIndex], randomnessIndex,
+     outByte))
+   else:
+    outByteText = chr(outByte)
+    if outByteText not in string.printable:
+     outByteText = "."
+    print(". 0x%02X (%03d) ^ 0x%02X (%03d) = %c 0x%02X" % (
+     inData[inDataIndex], inDataIndex,
+     randomnessDigest[randomnessIndex], randomnessIndex,
+     outByteText, outByte))
+   inDataIndex += 1
+   randomnessIndex += 1
+
+ return "".join(outData)
 
 
-print(binascii.hexlify("".join(outData).encode()))
+outData1 = crypt(secret, inData, True).encode("latin_1")
+outData2 = crypt(secret, outData1, False).encode("latin_1")
+
+print(binascii.hexlify(outData1))
+print(binascii.hexlify(outData2))
+print(inData == outData2)
